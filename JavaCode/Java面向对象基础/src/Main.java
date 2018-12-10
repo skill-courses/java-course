2de520186a373104f6d67dc2b72309636d0600d0
李景华ST800198 <lijinghua@odc.cmbchina.cn>
2018-12-10 19:51:04 +0800

    create class and object example

---
+++ JavaCode/Java面向对象基础/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person zhangsan = new Person("zhangsan", 18);
+        System.out.println(zhangsan.name);
+        System.out.println(zhangsan.getAge());
+    }
+}
