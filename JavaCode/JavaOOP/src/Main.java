d2b91fd20c1e4eb143aab60f542245f76df63a72
李景华ST800198 <lijinghua@odc.cmbchina.cn>
2018-12-11 11:49:08 +0800

    complete package

---
+++ JavaCode/JavaOOP/src/Main.java
@@ -0,0 +1,10 @@
+import model.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("zhangsan");
+        System.out.println(student.getName());
+    }
+}
--- JavaCode/Java面向对象基础/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Person zhangsan = new Person("zhangsan", 18);
-        System.out.println(zhangsan.name);
-        System.out.println(zhangsan.getAge());
-    }
-}
